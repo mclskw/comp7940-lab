8a4f88f42d0e3c77febab3f9d484f5df4766f5c6
mclskw <mclskw@gmail.com>
2025-02-08 18:32:32 +0800

    reorganise

---
+++ lab1/ex1.py
@@ -0,0 +1,8 @@
+# Find the all factors of x using a loop and the operator %
+# % means find remainder, for example 10 % 2 = 0; 10% 3 = 1
+x = 52633
+for i in range(x+1):
+	try:
+		if x%i==0:
+			print (i)
+	except: print("error when divided by", i)
---
+++ lab1/ex3.py
@@ -0,0 +1,6 @@
+# Write a program that be able to find all factors of the numbers in the list l
+l = [52633, 8137, 1024, 999]
+import ex2
+for i in l:
+	print("factor of" ,i)
+	ex2.print_factor(i)
