ee506ec099f7698f20c8743cf6fd43eea371041a
mclskw <mclskw@gmail.com>
2025-03-15 13:47:38 +0800

    bugfix

--- ChatGPT_HKBU.py
+++ ChatGPT_HKBU.py
@@ -2,7 +2,7 @@
 import os
 import requests
 class HKBU_ChatGPT():
-    def __init__(self,config_='./config.ini'):
+    def __init__(self):
         # Read configuration from environment variables
         self.basic_url = os.environ.get('CHATGPT_BASICURL')
         self.model_name = os.environ.get('CHATGPT_MODELNAME')
@@ -13,7 +13,7 @@ class HKBU_ChatGPT():
         conversation = [{"role": "user", "content": message}]
         url = f"{self.basic_url}/deployments/{self.model_name}/chat/completions/?api-version={self.api_version}"
         headers = { 'Content-Type': 'application/json',
-        'api-key': (self.config['CHATGPT']['ACCESS_TOKEN']) }
+        'api-key': self.access_token }
         payload = { 'messages': conversation }
         response = requests.post(url, json=payload, headers=headers)
         if response.status_code == 200:
